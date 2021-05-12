3ce5ea7de60d89354b621212ba984609aa016e4d
דניאל קריטי <danielkariti@hotmail.com>
2021-05-12 22:40:04 +0300

    Error handling added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import {Routes , RouterModule} from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -20,6 +22,8 @@ const routes :Routes = [
             {path: 'messages', component: MessagesComponent},
         ]
     },
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'not-found', component:NotFoundComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -24,9 +24,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
       this.toastr.success('Logged in successfully');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
