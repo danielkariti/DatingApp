b4080235e9f2a535d85ba9d6d46d9d07275d6fb7
דניאל קריטי <danielkariti@hotmail.com>
2021-05-12 18:56:22 +0300

    added register method

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,24 +10,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model:any = {}
-  loggedIn: boolean;
+  
 
-  constructor(private accountService: AccountService) { }
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    
   }
 
 
   login(){
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-      this.loggedIn = true;
     }, error => {
-      console.log(error.error);
+      console.log(error);
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
