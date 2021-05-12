7ba78efac905f33c44d8a40e8ddaaab677e8854a
דניאל קריטי <danielkariti@hotmail.com>
2021-05-12 17:16:50 +0300

    added UI navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,23 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    FormsModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
